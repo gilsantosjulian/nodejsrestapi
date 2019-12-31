95756e32281438c8cd2d5d7b85bbac905d2e7851
Julian <julian@minka.io>
2019-12-31 12:15:03 -0500

    feat(pacjage.json) add body-parser, compression, helmet, morgan and mongoose

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import express from 'express';
+import constants from '../config/constants';
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, err => {
+app.listen(constants.PORT, err => {
   if (err) {
     throw err;
   } else {
-    console.log(`Server running on port: ${
-      PORT
-    }-- - Running on ${
-      process.env.NODE_ENV
-    }-- - Make something great!`)
+    console.log(`Server running on port: ${constants.PORT} --- Running on ${process.env.NODE_ENV} --- Make something great.!`)
   }
 });
