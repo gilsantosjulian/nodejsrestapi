26e204660cf434c487b7a9fbfcb83bc522dd7b6d
Julian <julian@minka.io>
2020-01-03 13:40:32 -0500

    feat(index.js/user.routes) add routes for users module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-console */
 import express from 'express';
 import constants from '../config/constants';
 import '../config/database';
-import middlewareConfig from '../config/middleware';
-const app = express(); //passing the app instance to middlewareConfig 
+import middlewaresConfig from '../config/middleware';
+import apiRoutes from './modules';
 
-middlewareConfig(app);
+const app = express();
+middlewaresConfig(app);
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+apiRoutes(app);
 app.listen(constants.PORT, err => {
   if (err) {
     throw err;
   } else {
-    console.log(`Server running on port: ${constants.PORT} --- Running on ${process.env.NODE_ENV} --- Make something great.!`)
+    console.log(` Server running on port: ${constants.PORT} --- Running on ${process.env.NODE_ENV} --- Make something great `);
   }
 });
