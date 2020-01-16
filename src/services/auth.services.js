7d7abeca5fb4c848bcd59cf7981d8203c43c42e6
Julian <julian@minka.io>
2020-01-16 17:05:56 -0500

    core(auth.service) add JWTStrategy

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,18 +1,46 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import {
+  Strategy as JWTStrategy,
+  ExtractJwt
+} from 'passport-jwt';
 import User from '../modules/users/user.model';
+import constants from '../../config/constants';
 
 const localOpts = {
-  usernameField: 'email',
+  usernameField: 'email'
 };
-const localStrategy = new LocalStrategy(localOpts, async (email, password, done) => {
+const localStrategy = new LocalStrategy(
+  localOpts,
+  async (email, password, done) => {
+    try {
+      const user = await User.findOne({
+        email
+      });
+      if (!user) {
+        return done(null, false);
+      } else if (!user.authenticateUser(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (e) {
+      return done(e, false);
+    }
+  }
+);
+
+// Jwt strategy
+const jwtOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+  secretOrKey: constants.JWT_SECRET
+};
+
+const jwtStrategy = new JWTStrategy(jwtOpts, async (payload, done) => {
   try {
-    const user = await User.findOne({
-      email,
-    });
+    // Identify user by ID
+    const user = await User.findById(payload._id);
+
     if (!user) {
-      return done(null, false);
-    } else if (!user.authenticateUser(password)) {
       return done(null, false);
     }
     return done(null, user);
@@ -22,7 +50,11 @@ const localStrategy = new LocalStrategy(localOpts, async (email, password, done)
 });
 
 passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 export const authLocal = passport.authenticate('local', {
-  session: false,
+  session: false
+});
+export const authJwt = passport.authenticate('jwt', {
+  session: false
 });
