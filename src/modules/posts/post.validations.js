c2d0187ceb687ede8906fe8f1f18b62261298051
Julian <julian@minka.io>
2020-01-27 17:12:43 -0500

    core(post.validations) change the post validations fields

--- src/modules/posts/post.validations.js
+++ src/modules/posts/post.validations.js
@@ -4,11 +4,8 @@ export const passwordReg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 export default {
   createPost: {
     body: {
-      email: Joi.string().email().required(),
-      password: Joi.string().regex(passwordReg).required(),
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      userName: Joi.string().required(),
+      title: Joi.string().required(),
+      text: Joi.string().required(),
     },
   },
 };
