8da5d08fe9ff25456e01db5a965a33ddce3cd9ed
Julian <julian@minka.io>
2020-01-31 07:35:46 -0500

    feat(post.controllers) get user on getPostById handle method

--- src/modules/posts/post.controllers.js
+++ src/modules/posts/post.controllers.js
@@ -12,7 +12,7 @@ export async function createPost(req, res) {
 
 export async function getPostById(req, res) {
   try {
-    const post = await Post.findById(req.params.id);
+    const post = await Post.findById(req.params.id).populate('user');
     return res.status(HTTPStatus.OK).json(post);
   } catch (e) {
     return res.status(HTTPStatus.BAD_REQUEST).json(e);
