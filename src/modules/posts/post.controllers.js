20ac846bae1f5c4a0b0ad77197bea89846e83a14
Julian <julian@minka.io>
2020-02-18 21:55:26 -0500

    feat(post.controller) add updatePost handle method

--- src/modules/posts/post.controllers.js
+++ src/modules/posts/post.controllers.js
@@ -1,25 +1,25 @@
 import HTTPStatus from 'http-status';
 import Post from './post.model';
 
-export async function createPost(req, res) {
+export const createPost = async (req, res) => {
   try {
     const post = await Post.createPost(req.body, req.user._id);
     return res.status(HTTPStatus.CREATED).json(post);
   } catch (e) {
     return res.status(HTTPStatus.BAD_REQUEST).json(e);
   }
-}
+};
 
-export async function getPostById(req, res) {
+export const getPostById = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id).populate('user');
     return res.status(HTTPStatus.OK).json(post);
   } catch (e) {
     return res.status(HTTPStatus.BAD_REQUEST).json(e);
   }
-}
+};
 
-export async function getPostsList(req, res) {
+export const getPostsList = async (req, res) => {
   const limit = parseInt(req.query.limit, 0);
   const skip = parseInt(req.query.skip, 0);
   try {
@@ -28,4 +28,20 @@ export async function getPostsList(req, res) {
   } catch (e) {
     return res.status(HTTPStatus.BAD_REQUEST).json(e);
   }
-}
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.user.equals(req.user._id)) {
+      return res.sendStatus(HTTPStatus.UNAUTHORIZED);
+    }
+    Object.keys(req.body).forEach(key => {
+      post[key] = req.body[key];
+    });
+    return res.status(HTTPStatus.OK).json(await post.save());
+  } catch (e) {
+    return res.status(HTTPStatus.BAD_REQUEST).json(e);
+  }
+};
+
