90064198f3ccca156c2e45b487dcdd1e7154e6bb
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-06-18 19:19:09 -0500

    refactor(post.model/post.controller/user.model): move post model to db folder

--- src/modules/db/mongo/posts/post.model.js
+++ src/modules/db/mongo/posts/post.model.js
@@ -2,43 +2,42 @@ import mongoose, { Schema } from 'mongoose';
 import slug from 'slug';
 import uniqueValidator from 'mongoose-unique-validator';
 
-const PostSchema = new Schema(
-  {
-    title: {
-      type: String,
-      trim: true,
-      required: [true, 'Title   is required!'],
-      minlength: [3, 'Title   need to be longer!'],
-      unique: true,
-    },
-    text: {
-      type: String,
-      trim: true,
-      required: [true, 'Text   is required!'],
-      minlength: [10, 'Text   need to be longer!'],
-    },
-    slug: {
-      type: String,
-      trim: true,
-      lowercase: true,
-    },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-    },
-    favoriteCount: {
-      type: Number,
-      default: 0,
-    },
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Title   is required!'],
+    minlength: [3, 'Title   need to be longer!'],
+    unique: true,
   },
-  { timestamps: true },
-);
+  text: {
+    type: String,
+    trim: true,
+    required: [true, 'Text   is required!'],
+    minlength: [10, 'Text   need to be longer!'],
+  },
+  slug: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  favoriteCount: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true
+}, );
 
 PostSchema.plugin(uniqueValidator, {
   message: '{VALUE} already taken!',
 });
 
-PostSchema.pre('validate', function(next) {
+PostSchema.pre('validate', function (next) {
   this._slugify();
 
   next();
@@ -62,15 +61,14 @@ PostSchema.methods = {
 };
 
 PostSchema.statics = {
-  createPost(args, user) {
-    return this.create({
-      ...args,
-      user,
-    });
-  },
-  list({ skip = 0, limit = 5 } = {}) {
+  list({
+    skip = 0,
+    limit = 5
+  } = {}) {
     return this.find()
-      .sort({ createdAt: -1 })
+      .sort({
+        createdAt: -1
+      })
       .skip(skip)
       .limit(limit)
       .populate('user');
