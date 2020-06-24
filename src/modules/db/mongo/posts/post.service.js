237b7a88acae7db31fa2c02e8397db2d53567450
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-06-23 21:36:06 -0500

    feat(post.service): add remve method

--- src/modules/db/mongo/posts/post.service.js
+++ src/modules/db/mongo/posts/post.service.js
@@ -21,7 +21,27 @@ const getById = async (id) => {
   return post.toJSON()
 }
 
+const remove = async (id) => {  
+  const post = await PostSchema.findById(id)
+  if(!post) {
+    console.log('[redis:remove]: not found in db');
+    return null
+  }
+  const postRemoved = await post.remove()
+  console.log('[redis:remove]: removing from db');
+
+  if (!postRemoved) {
+    console.log('[redis:remove]: not found in redis');
+    return null
+  }
+
+  const memPost = await redisMem.remove(id)
+  console.log('[redis:remove]: removing from redis');
+  return memPost
+}
+
 module.exports = {
   create,
   getById,
+  remove,
 }
